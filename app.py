da3bbebbb6c3446d95f42c0e9470d5662dbad411
Ahmed Awad <66866923+aaburakhia@users.noreply.github.com>
2025-10-01 13:03:52 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Care-AI Analytics",
+    page_icon="🩺",
+)
+
+st.title("Care-AI: The Integrated Health Co-Pilot")
+st.write("Welcome to the future of personal health management.")
